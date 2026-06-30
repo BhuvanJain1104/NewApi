b8fd3e1df05719b98a79d6e5000c2f1a0cc460f3
BhuvanJain1104 <jain.bhuvancr7@gmail.com>
2026-06-30 22:44:25 +0530

    NewsMonkey Final Version

--- server/app.js
+++ server/app.js
@@ -1,38 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/auth");
 const bookmarkRoutes = require("./routes/bookmark");
 const newsRoutes = require("./routes/news");
 const userRoutes = require("./routes/user");
 const preferenceRoutes = require("./routes/preferences");
+
 dotenv.config();
-console.log("URI starts with:", process.env.MONGO_URI.substring(0, 60));
-// Connect Database
+
 connectDB();
 
 const app = express();
-
-const cors = require("cors");
 
 app.use(
   cors({
     origin: [
       "http://localhost:3000",
       "https://news-monkey-gold-one.vercel.app",
-      "https://news-monkey-ps400rosy-jain4.vercel.app"
+      "https://news-monkey-ps400rosy-jain4.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/bookmarks", bookmarkRoutes);
 app.use("/api/news", newsRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/preferences", preferenceRoutes);
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -43,5 +46,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  
+  console.log(`Server running on ${PORT}`);
 });
