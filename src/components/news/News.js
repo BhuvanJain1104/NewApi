1d6b665231b58290da3cee97b487624bf72dd34a
BhuvanJain1104 <jain.bhuvancr7@gmail.com>
2026-06-30 22:08:56 +0530

    Fix deployment issues and update API configuration

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -64,7 +64,7 @@ export class News extends Component {
           this.props.searchQuery
         )}&page=1&pageSize=${this.props.pageSize}`;
       } else {
-        url = `${process.env.REACT_APP_API}/news?country=${this.props.country}&category=${this.props.category}&page=1&pageSize=${this.props.pageSize}`;
+        url = `https://newsmonkey-1xh6.onrender.com/api/news?country=${this.props.country}&category=${this.props.category}&page=1&pageSize=${this.props.pageSize}`;
       }
 
       this.props.setProgress(50);
